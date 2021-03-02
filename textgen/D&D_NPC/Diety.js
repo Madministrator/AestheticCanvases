9a0345c2273011d78fc99ad88b655500fa71b670
Madministrator <vhevan.927@gmail.com>
2021-03-01 22:30:12 -0600

    expanded the generators vocabulary
    haven't made enough templates, but I think this will be the start of a more complex series of sentences

---
+++ textgen/D&D_NPC/Diety.js
@@ -0,0 +1,43 @@
+const dieties = `
+#diety:
+---- Forgotten Realms ----
+Auril, goddess of winter
+Azuth, god of wizards
+Bane, god of tyranny
+Beshaba, goddess of misfortune
+Bhall, god of murder
+Chauntea, goddess of agriculture
+Cyric, god of lies
+Deneir, god of writing
+Eldath, goddess of peace
+Gond, god of craft
+Helm, god of protection
+Ilmater, god of endurance
+Kelemvor, god of the dead
+Lathander, god of birth and renewal
+Leira, goddess of illusion
+Lliira, goddess of joy
+Loviatar, goddess of pain
+Malar, god of the hunt
+Mask, god of theives
+Mielikki, goddess of forests
+Milil, god of poetry and song
+Myrkul, god of death
+Mystra, goddess of magic
+Oghma, god of knowledge
+Savras, god of divination and fate
+Selune, goddess of the moon
+Shar, goddess of darkness and loss
+Silvanus, god of wild nature
+Sune, goddess of love and beauty
+Talona, goddess of disease and poison
+Talos, god of storms
+Tempus, god of war
+Torm, god of courage and self-sacrifice
+Tymora, goddess of good fortune
+Tyr, god of justice
+Umberlee, goddess of the sea
+Waukeen, goddess of trade
+
+-- TODO: Dieties of Greyhack, Dragonlance, Eberron, and other non-human dieties
+`
--- textgen/D&D_NPC/NpcTraits.js
+++ textgen/D&D_NPC/NpcTraits.js
@@ -2,311 +2,10 @@ const npcTraits = `
 - for reference, any line which starts with a hyphen is treated as a comment
 
 #template:
-|name|, a |alignment| |race| |gender|
+<a> |gender| |race| catches your eye. They are [tall, small, average height] for their race, and most noticably they have <a> |distinguishing feature|.
 You see <a> |race| named |name|, who is <a> |player class|
 Sitting across from you at the table you see <a> |race| |gender|; the most interesting part about their appearance is that they have <a> |distinguishing feature|.
-
-#name:
-Winestock
-Syndelle
-LaBorde
-Cowage
-Kiefer
-Malezar
-Vargus
-Coyle
-Kissane
-Elvidge
-Rylance
-Bristow
-Slatter
-Blewitt
-Symons
-Charlton
-Brims
-Plooy
-Golenko
-Keogh
-Hendry
-Whetter
-Stamey
-Gradwell
-Manon
-Fazeli
-Capkun
-Cainus
-Cardone
-Chancellor
-Yellin
-Linton
-Segler
-Maratta
-Carbanero
-Chambers
-Guray
-Littlemore
-Chubbs
-Huey
-Lawton
-Letner
-Wroe
-Melancon
-Sahin
-Heri
-Javed
-Molowitch
-Maity
-Livi
-Gronbeck
-Manjoot
-Roesch
-Ozden
-Harrop
-Grunstand
-Fliss
-Toomey
-Pron
-Blundy
-Horne
-Tarrant
-Whitelocke
-Hibbard
-Northover
-Westwood
-Brunner
-Byrne
-Danby
-Keevan
-Fife
-Fares
-Hefferman
-Smiff
-Wareham
-Amali
-Jasin
-Elmes
-Lam
-Durkee
-Pinder
-Mehl
-Yana
-Guntis
-Rippon
-Clave
-Riwaka
-Critichety
-Skene
-Huckle
-Eveleigh
-Grist
-Dizon
-Punch
-Dobner
-Rixon
-Corley
-Grati
-Valka
-Shawcross
-Exton
-Graff
-Denton
-Beers
-Vistan
-Debose
-Khamedoost
-Velasco
-Cossey
-Ditko
-Gruenwald
-Buscema
-Ineson
-Thebeau
-Poirier
-Framestone
-Spruce
-Bardak
-Duprat
-Borgstrum
-Houte
-Rath
-Yip
-Horsfield
-Penhale
-Saturnyne
-Wesche
-Malenfant
-Schott
-Bomsteen
-Wheeldin
-Kullmark
-Mellycott
-Denker
-Warworst
-Hassell
-Dobson
-Coxon
-Eccelston
-Leenster
-Boole
-Allnutt
-Vaughnan
-Wiltshire
-Senn
-Willoughby
-Comerford
-Aidan
-Horey
-Fredland
-Bagg
-Rosco
-Guinery
-Lindsell
-Yasca
-Horik
-Ralf
-Maxwell
-Gilray
-Gribbon
-Crowther
-Crowder
-Boyd
-Jahke
-Jolliffe
-Komene
-Cordella
-Shakes
-Traetta
-Peppard
-Verling
-Bray
-Zemetis
-Whelan
-Davery
-Baird
-Moxam
-Seffrin
-Crombie
-Lillis
-Garside
-Whitby
-Carmalis
-Stripp
-Miglett
-Hooten
-Alarcon
-Gravett
-Hardinger
-Gyerman
-Marfleet
-Sawtell
-Strobel
-Gunnarson
-Wilding
-Clappison
-Mandt
-Kjell
-Cant
-Stussy
-Ballantyne
-Saturno
-Kern
-Rhinehart
-Null
-Durkeep
-Chiwetel
-Mads
-Landecker
-Pye
-Graze
-Hoppeton
-Featherstone
-Monteith
-Ironshields
-Lumley
-Bardsley
-Baumgartner
-Tovar
-Lecards
-Lasky
-Nagle
-Bunnag
-Darrow
-Knue
-Elba
-Hamish
-Valera
-Amanti
-Hurst
-Rinna
-Onyekyere
-Saric
-Fendick
-Kosta
-Skovli
-Callum
-Ridge
-Archer
-Tendolle
-Kirkwood
-Coulam
-Raki
-Ballantine
-Zebulon
-Eggins
-Pommy
-Jonay
-Kiri
-Grassi
-Brough
-Connors
-Bowler
-Aird
-Sorby
-Haystacks
-Dare
-Parkal
-Suna
-Struth
-Romita
-Abnett
-Nims
-Farkas
-Darch
-Malik
-Kornel
-Faulkes
-Horley
-Trezzi
-Newbury
-Hoddins
-Loddins
-Braga
-Morley
-Hackl
-Calle
-Boomsville
-Razo
-Majors
-Habberstad
-Brock
-Newirth
-Coughlin
-Phate
-Ledbetter
-Fendick
-Wayan
-Rook
-Gunn
-Binns
-Endicott
-Pennycooke
-Shed
-Alphona
-Pettigrove
-Hasche
-Kurple
-Nittmann
-Jannic
+Standing out from the others around you there is a |gender| |race| cleric. Dangling from their neck is a holy symbol, a flat metal disk which has been stamped with the symbol of |diety|.
 
 #alignment:
 lawful good
@@ -371,6 +70,23 @@ burn scar which covers their left eye, leaving them with a squint on that side
 eyepatch covers their [left, right] eye
 tattoo of <a> |tattoo|
 faint smell of decay about them
+long glorious beard that reaches their waist
+missing tooth which doesn't prevent them from making a wide smile
+arm that has a different skin tone, length, and general shape than the other
+series of scars and callouses that appear over any exposed skin, perhaps the result of many brutal sparring sessions
+tail that for all intents and purposes they shouldn't have
+pair of havy bags under their eyes, indicative of an inability to sleep well
+complete lack of hair on their body
+interesting pair of eyes, their [left, right] eye is brown, while the other is |dragon color|
+acid scar on the back of their [left, right] hand
+scar around their neck
+pair of thick spectacles that don't fit
+one eye swollen over from a recent fight
+beard that is visibly fake
+holy symbol of |diety| branded onto their chest, presumably with a hot iron
+mouthful of metal teeth, not all of which are of the same metal
+forked tongue
+missing [left, right] ear
 
 #tattoo:
 octopus reaching around their [left, right] forearm
@@ -378,4 +94,83 @@ all-seeing eye on the back of their [neck, left hand, right hand]
 ship's anchor on their [right, left] shoulder
 dagger on the back of their [left, right] calf
 |dragon color| colored scale that is almost lifelike in detail
+series of draconic runes on their forehead. Anyone who investigates it falls victim to a suggestion spell as it reads "Stop looking at my forehead"
+map of an oddly shapped island with a red X on one of its shores
+arrow wrapping around their [left, right] wrist
+arrow stretching down their inner forearm
+skull on their [left arm, right arm, neck, chest, left ankle, right ankle]
+snake coiling up their [left, right] arm
+spiderweb on their [left, right] shoulder
+heart on their chest
+rose, stem and all, on their neck
+ring of thorns which wraps around their neck
+mermaid, languishing on a rock on their [left, right] arm
+|dragon color| dragon which takes up their entire back
+
+#hairstyle:
+[thick, wispy, straight, wavy, curly, wiry, oily, lush, poofy, greasy, unruly, short-cropped] hair
+long braids coiled tight against their head
+locks curled in such a way that you assume must be high-maintence
+
+#height:
+unusually short
+short in stature
+[average, slightly above average, well above average] height
+
+#physical build:
+thin and delicate
+of average build
+well-muscled
+slightly overweight
+rather overweight
+lean and lanky
+lithe and lean
+thin and wiry
+sinewy and strong
+flabby and weak
+thin and flimsy
+soft and chubby
+thin and petite
+pudgy, to be polite
+big and broad
+stocky and strong
+bony
+wide and ponderous
+covered in hair
+
+#jewelry:
+single earing on the [left, right] ear
+pair of earings
+[small, large] chain about the neck
+brooch with a family crest of sorts
+ring on their [left, right] [pinky, ring, middle, index] finger
+ring on their [left, right] thumb
+several rings across both of their hands
+bracelet around their [left, right] wrist
+medallion
+ornate belt buckle
+
+#gemstone:
+amethyst
+crystal
+diamond
+emerald
+jade
+obsidian
+opal
+pearl
+ruby
+sapphire
+topaz
+turqoise
+
+#metal:
+steel
+bronze
+pewter
+copper
+silver
+electrum
+gold
+platinum
 `
